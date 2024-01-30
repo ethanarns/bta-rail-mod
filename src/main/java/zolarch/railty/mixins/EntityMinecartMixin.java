202c8a24bd8785fa584aca004d8d59f2b94b774f
Ethan Messinger-Arns <ethanarns95@gmail.com>
2024-01-29 19:28:57 -0800

    Removed FastRail, changed name to Railty

--- src/main/java/zolarch/railty/mixins/EntityMinecartMixin.java
+++ src/main/java/zolarch/railty/mixins/EntityMinecartMixin.java
@@ -1,38 +1,21 @@
-package zolarch.railmod.mixins;
+package zolarch.railty.mixins;
 
-import net.minecraft.core.block.BlockRail;
 import net.minecraft.core.entity.Entity;
 import net.minecraft.core.entity.EntityItem;
 import net.minecraft.core.entity.vehicle.EntityMinecart;
-import net.minecraft.core.item.Item;
 import net.minecraft.core.item.ItemStack;
-import net.minecraft.core.util.helper.MathHelper;
 import net.minecraft.core.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.ModifyConstant;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import zolarch.railmod.RailMod;
+import zolarch.railty.Railty;
 
 import java.util.List;
 
 @Mixin(value = EntityMinecart.class, remap = false)
 public abstract class EntityMinecartMixin extends Entity {
-	@Shadow
-	public abstract void remove();
-
-	@Unique
-	public int fastCountDown = 0;
-
-	@Unique
-	public double lastAbsVelX = 0;
-
-	@Unique
-	public double lastAbsVelZ = 0;
 
 	@Shadow
 	public int minecartType;
@@ -40,27 +23,8 @@ public abstract class EntityMinecartMixin extends Entity {
 	@Shadow
 	private ItemStack[] cargoItems;
 
-	@Shadow
-	public abstract Entity ejectRider();
-
 	public EntityMinecartMixin(World world) {
 		super(world);
-	}
-
-	@ModifyConstant(method = "onUpdate2", constant = @Constant(doubleValue = 0.4))
-	private double speedMod(double input) {
-		int i = MathHelper.floor_double(this.x);
-		int j = MathHelper.floor_double(this.y);
-		int k = MathHelper.floor_double(this.z);
-		if (BlockRail.isRailBlockAt(this.world, i, j - 1, k)) {
-			--j;
-		}
-		int l = this.world.getBlockId(i, j, k);
-		if (l == RailMod.FAST_BLOCK_ID) {
-			this.fastCountDown = 2;
-			return 3.6;
-		}
-		return 0.4;
 	}
 
 	@Inject(method = "tick",at = @At("TAIL"))
@@ -69,7 +33,7 @@ public abstract class EntityMinecartMixin extends Entity {
 		if (this.minecartType == 1 && !this.world.isClientSide) {
 			List<Entity> nearby = this.world.getEntitiesWithinAABBExcludingEntity(this,this.bb.expand(0.1,0.2,0.1));
 			if (nearby != null && !nearby.isEmpty()) {
-				RailMod.LOGGER.debug("Nearby ChestCart entity count: " + nearby.size());
+				Railty.LOGGER.debug("Nearby ChestCart entity count: " + nearby.size());
                 for (int j = 0; j < nearby.size(); j++) {
 					if (!nearby.get(j).removed && nearby.get(j) instanceof EntityItem) {
 						EntityItem item = (EntityItem) nearby.get(j);
@@ -98,39 +62,5 @@ public abstract class EntityMinecartMixin extends Entity {
                 }
 			}
 		}
-		// Passenger minecart on a fast block
-		double xv = Math.abs(this.xd);
-		double zv = Math.abs(this.zd);
-		if (this.fastCountDown > 0) {
-			this.fastCountDown--;
-		}
-		if (this.minecartType == 0 && this.yd < 0.01 && this.fastCountDown > 0) {
-			RailMod.LOGGER.debug("On fast block " + xv + ", " + zv + " (lasts: " + this.lastAbsVelX + ", " + lastAbsVelZ);
-			if (this.lastAbsVelX < 0.001 && this.lastAbsVelZ < 0.001) {
-				// Just started, do nothing
-				RailMod.LOGGER.debug("Just started");
-			} else {
-				if (xv > 0.001 && this.lastAbsVelX < 0.001) {
-					RailMod.LOGGER.debug("Too fast a turn X!");
-					if (this.passenger != null) {
-						this.passenger.animateHurt();
-						this.ejectRider();
-					}
-					this.remove();
-					this.spawnAtLocation(Item.minecart.id, 1, 0.0F);
-				}
-				if (zv > 0.001 && this.lastAbsVelZ < 0.001) {
-					RailMod.LOGGER.debug("Too fast a turn Z!");
-					if (this.passenger != null) {
-						this.passenger.animateHurt();
-						this.ejectRider();
-					}
-					this.remove();
-					this.spawnAtLocation(Item.minecart.id, 1, 0.0F);
-				}
-			}
-		}
-		this.lastAbsVelX = xv;
-		this.lastAbsVelZ = zv;
 	}
 }
