3e971e3aa3615cfbafe90bd13e0122bee7933615
Ethan Messinger-Arns <ethanarns95@gmail.com>
2024-01-31 21:05:33 -0800

    Remove pop sound

--- src/main/java/zolarch/railty/mixins/EntityMinecartMixin.java
+++ src/main/java/zolarch/railty/mixins/EntityMinecartMixin.java
@@ -29,8 +29,7 @@ public abstract class EntityMinecartMixin extends Entity {
 
 	@Inject(method = "tick",at = @At("TAIL"))
 	private void chestMinecartPickupItem(CallbackInfo ci) {
-		// Ensure it's a chest minecart (and handle server side)
-		boolean shouldPlayPop = false;
+		// Ensure it's a chest minecart
 		if (this.minecartType == 1 && !this.world.isClientSide) {
 			List<Entity> nearby = this.world.getEntitiesWithinAABBExcludingEntity(this,this.bb.expand(0.1,0.2,0.1));
 			if (nearby != null && !nearby.isEmpty()) {
@@ -45,7 +44,9 @@ public abstract class EntityMinecartMixin extends Entity {
 									// It *can* be incremented, do so
 									this.cargoItems[i].stackSize++;
 									item.remove();
-									shouldPlayPop = true;
+									// Doesn't work on servers, they don't work that way
+//									this.world.playSoundAtEntity(this, "random.pop", 0.2F,
+//										((this.random.nextFloat() - this.random.nextFloat()) * 0.7F + 1.0F) * 2.0F);
 									break;
 								}
 								// Otherwise, go to the next slot in line, this one is full
@@ -53,17 +54,12 @@ public abstract class EntityMinecartMixin extends Entity {
 								// Found an empty slot, and there was not a duplicate earlier
 								this.cargoItems[i] = item.item.copy();
 								item.remove();
-								shouldPlayPop = true;
                                 break;
                             }
                         }
 					}
                 }
 			}
-		}
-		if (shouldPlayPop) {
-			this.world.playSoundAtEntity(this, "random.pop", 0.2F,
-				((this.random.nextFloat() - this.random.nextFloat()) * 0.7F + 1.0F) * 2.0F);
 		}
 	}
 }
